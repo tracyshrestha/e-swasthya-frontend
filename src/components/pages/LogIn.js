51e4ce8a8910ffb2b3371ca8fa37ec413e41aff2
Tracyshrestha <tracyyyshrestha@gmail.com>
2023-05-13 21:16:54 +0545

    make mobile responsive and correct textfield error

--- src/components/pages/LogIn.js
+++ src/components/pages/LogIn.js
@@ -11,12 +11,12 @@ function LogIn() {
       <div className='fixed inset-0  h-screen backdrop-blur-sm flex  flex-col justify-center items-center bg-[#E6F4F9] '>
      
         <div className='flex justify-center items-center '>
-        <img src={logo} alt="" class="self-center h-[60px]" />
+        <img src={logo} alt="" class="self-center h-[60px] pt-2" />
           </div>
-        <form className='h-[550px] w-[600px] items-center rounded-sm border-[2px] border-[#f8f8f8] p-9 m-9 mt-4 mx-24 bg-white'>
+        <form className='sm:h-[550px] sm:w-[600px] h-[505px] w-[350px] items-center rounded-sm border-[2px] border-[#f8f8f8] p-9 m-9 mt-4 mx-24 bg-white '>
           <div className='items-center'>
-            <h2 className='text-[30px] font-bold pt-1 pb-1 px-[155px]  text-gray-700'>Welcome Back</h2>
-            <h6 className='text-l px-[100px] pb-9 text-gray-400'>Enter your credintials to access your account.</h6>
+            <h2 className='sm:text-[30px] text-[22px] font-bold sm:pt-1  pb-1 sm:px-[155px] px-[60px]  text-gray-700'>Welcome Back</h2>
+            <h6 className='sm:text-l text-[11px] sm:px-[100px] mx-7 sm:pb-9 pb-5 text-gray-400'>Enter your credintials to access your account.</h6>
           </div>
 
           <div class=" pb-6">
@@ -24,7 +24,7 @@ function LogIn() {
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <img src={email} alt="" class="self-center w-5 h-5 rounded-full mx-1" />
             </div>
-            <input type="text" id="email-address-icon" class="0 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email"></input>
+            <input type="text" id="email-address-icon" class="0 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  " placeholder="Enter your email"></input>
           </div>
           </div>
 
@@ -32,11 +32,11 @@ function LogIn() {
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <img src={lock} alt="" class="self-center w-5 h-5 rounded-full mx-1" />
             </div>
-            <input type="text" id="email-address-icon" class="0 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your password"></input>
+            <input type="text" id="email-address-icon" class="0 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  " placeholder="Enter your password"></input>
           </div>
 
           <div className='flex flex-col items-center'>
-            <button className='w-[520px] text-center py-3 mt-8 font-bold  bg-[#42ADF0] hover:bg-[#4D6B9C] relative rounded-xl text-white hover:bold'>
+            <button className='sm:w-[520px] w-[275px] text-center py-3 mt-8 font-bold  bg-[#42ADF0] hover:bg-[#4D6B9C] relative rounded-xl text-white hover:bold'>
               Sign In
             </button>
           </div>
@@ -47,16 +47,16 @@ function LogIn() {
           </div>
 
           <p>
-            <button className='flex items-center items-center  flex-col mb-4 ml-[160px] rounded-md hover:bg-[#E6F4F9] '>
-              <span class="inline-flex items-baseline  relative p-3 rounded-md border-[2px] border-[#d4d2d2] w-[200px]">
+            <button className='flex items-center items-center  flex-col mb-4 sm:ml-[160px] ml-12 rounded-md hover:bg-[#E6F4F9] '>
+              <span class="inline-flex items-baseline  relative p-3 rounded-md border-[2px] border-[#d4d2d2] sm:w-[200px] w-[172px] sm:text-[17px] text-[13px]">
                 <img src={google} alt="" class="self-center w-5 h-5 rounded-full mx-1" />
                 Log in with Google
               </span>
             </button>
           </p>
 
-          <div className=' text-center mt-[25px] text-gray-500 '>
-            <p className='text-l text-center '>Don't have an account?  <a className='text-lg font-bold relative text-[#42ADF0] hover:text-[#4D6B9C] underline hover:decoration-solid   hover:uppercase' href="LogInUser">Sign up.</a></p>
+          <div className=' text-center sm:mt-[25px] mt-5 text-gray-500 '>
+            <p className='sm: text-l text-[14px] text-center '>Don't have an account?  <a className='sm:text-lg text-[16px] font-bold relative text-[#42ADF0] hover:text-[#4D6B9C] underline hover:decoration-solid   hover:uppercase' href="LogInUser">Sign up.</a></p>
 
           </div>
         </form>
