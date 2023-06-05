f28e033e27bae6063e7572b25d689ebae9722e6d
Tracyshrestha <tracyyyshrestha@gmail.com>
2023-06-05 05:45:28 +0545

    add multiple fie upload option

--- src/components/pages/Reports/ReportTest.js
+++ src/components/pages/Reports/ReportTest.js
@@ -8,6 +8,31 @@ function ReportTest() {
 
   })
 
+  const [files, setFiles] = useState([]);
+
+  const handleFileChange = (event) => {
+    const selectedFiles = Array.from(event.target.files);
+    setFiles(selectedFiles);
+  };
+
+  const handleRemoveFile = (index) => {
+    const newFiles = [...files];
+    newFiles.splice(index, 1);
+    setFiles(newFiles);
+  };
+
+  const handleAddFiles = (event) => {
+    const selectedFiles = Array.from(event.target.files);
+    setFiles((prevFiles) => [...prevFiles, ...selectedFiles]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle file upload logic here
+    // send the files to the server or perform any other action
+    console.log('Files:', files);
+  };
+
   const inputHandle = (e) => {
     setState({
       ...state,
@@ -21,7 +46,7 @@ function ReportTest() {
 
   return (
 
-    <div className="w-screen h-screen flex  justify-center items-center bg-[#E6F4F9]">
+    <div className="w-screen h-full flex  justify-center items-center bg-[#E6F4F9]">
       <div className="p-4 flex justify-center items-center">
         <form className="h-auto sm:w-[750px]  w-[350px] items-center rounded-sm  p-8 m-8 mt-1 mx-24  bg-white ">
           <div className="flex justify-center items-center ">
@@ -71,7 +96,7 @@ function ReportTest() {
 
                 <div className='pt-3'>
                   <h1 className='sm:text-[15px] text-[12px] pb-2 relative'>Select Date:</h1>
-                  <input
+                  <input  className="border border-gray-300 placeholder-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5"
                     type="date"
                     name="date"
                     value={selectedDate}
@@ -79,8 +104,25 @@ function ReportTest() {
                   />
 
                 </div>
+              
 
                 {/* file add garna bakhi */}
+
+                <div className='pt-3'>
+                <h1 className='sm:text-[15px] text-[12px] pb-2 relative'>Upload Files::</h1>  
+                <div className="border border-gray-300 placeholder-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-4">
+                  <input  required type="file" multiple onChange={handleAddFiles} />
+                  {/* <button onClick={handleSubmit}>Upload</button> */}
+                  <ul >
+                    {files.map((file, index) => (
+                      <li className="" key={index}>
+                        {file.name}
+                        <button className='border border-gray-500 rounded-lg my-1.5 p-1 px-3 ml-6  bg-[#e5f5ffef] ' onClick={() => handleRemoveFile(index)}>Remove</button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                </div>
 
                 <div className='flex flex-col items-center'>
                   <button type="submit" className='sm:w-[520px] rounded-md w-[275px] text-center py-3 mt-8 font-bold  bg-[#42ADF0] hover:bg-[#4D6B9C] relative  text-white hover:bold'>
